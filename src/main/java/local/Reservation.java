857067a58f84c71d0e6294f2ae4c6aeb5d12c6e5
juny5131 <juny5131@naver.com>
2020-09-02 16:22:11 +0900

    first commit

---
+++ src/main/java/local/Reservation.java
@@ -0,0 +1,81 @@
+package local;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Reservation_table")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long screeningId;
+    private String hospitalId;
+    private String hospitalNm;
+    private String custNm;
+    private String status;
+
+    @PostPersist
+    public void onPostPersist(){
+        ReservationCompleted reservationCompleted = new ReservationCompleted();
+        BeanUtils.copyProperties(this, reservationCompleted);
+        reservationCompleted.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        ReservationChanged reservationChanged = new ReservationChanged();
+        BeanUtils.copyProperties(this, reservationChanged);
+        reservationChanged.publishAfterCommit();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getScreeningId() {
+        return screeningId;
+    }
+
+    public void setScreeningId(Long screeningId) {
+        this.screeningId = screeningId;
+    }
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+    public String getHospitalNm() {
+        return hospitalNm;
+    }
+
+    public void setHospitalNm(String hospitalNm) {
+        this.hospitalNm = hospitalNm;
+    }
+    public String getCustNm() {
+        return custNm;
+    }
+
+    public void setCustNm(String custNm) {
+        this.custNm = custNm;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
